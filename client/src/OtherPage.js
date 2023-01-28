b0b56d901759be5a1dacd25240e3a817038b5032
szymi <szymon.czopek@wp.pl>
2023-01-28 18:48:41 +0100

    init

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      <p>Multifib</p>
+      <p>Szymon Czopek 2.1</p>
       <Link to="/">Go back home</Link>
     </div>
   );
